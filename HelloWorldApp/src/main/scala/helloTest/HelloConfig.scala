29f20f27575fd76e40b22a2063703f240786b7ac
Harshank Vengurlekar <vharshank@gmail.com>
2015-02-11 18:24:49 -0800

    Update HelloConfig.scala

--- HelloWorldApp/src/main/scala/helloTest/HelloConfig.scala
+++ HelloWorldApp/src/main/scala/helloTest/HelloConfig.scala
@@ -6,6 +6,7 @@ import org.springframework.stereotype._
 import org.springframework.web.bind.annotation._
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @Configuration
@@ -13,7 +14,7 @@ import org.springframework.context.annotation.ComponentScan
 @ComponentScan
 class HelloConfig {
 
-        @RequestMapping(Array("/"))
+        @RequestMapping(value=Array("/"), method=Array(RequestMethod.GET))
         @ResponseBody
         def home(): String = "Hello Guys!"
 }
